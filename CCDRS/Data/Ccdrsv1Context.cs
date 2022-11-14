64d433d6179c7a3a8f12f685698e30f29f3e8cb4
amitsandhel <amit.sandhel@gmail.com>
2022-11-14 13:26:42 -0500

    Direction Model and Service Added Added packages for postgresql and mongodb Added direction table as a model and service as generated by the EFCore

---
+++ CCDRS/Data/Ccdrsv1Context.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using CCDRS.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace CCDRS.Data;
+
+public partial class Ccdrsv1Context : DbContext
+{
+    public Ccdrsv1Context()
+    {
+    }
+
+    public Ccdrsv1Context(DbContextOptions<Ccdrsv1Context> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Direction> Directions { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Direction>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("direction_pkey");
+
+            entity.ToTable("direction");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Abbr)
+                .HasMaxLength(1)
+                .HasColumnName("abbr");
+            entity.Property(e => e.Compass).HasColumnName("compass");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ CCDRS/Model/Direction.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CCDRS.Model;
+
+public partial class Direction
+{
+    public int Id { get; set; }
+
+    public string Compass { get; set; } = null!;
+
+    public char Abbr { get; set; }
+}
