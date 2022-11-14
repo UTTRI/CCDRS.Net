f249162d78d5e2b6ae7338b5751bdd5d4f2b34c7
amitsandhel <amit.sandhel@gmail.com>
2022-11-14 13:35:59 -0500

    Added xml code to the Direction class and added an attribute name for abbr

--- CCDRS/Model/Direction.cs
+++ CCDRS/Model/Direction.cs
@@ -1,13 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CCDRS.Model;
-
+/// <summary>
+/// Class that maps to the postgresql direction table
+/// It has three fields 
+/// Id: Primary serial key
+/// Compass: Key that holds the name of direction eg North, South
+/// Abbr: An Abbreviation of the direction eg N, E, S, W
+/// </summary>
 public partial class Direction
 {
     public int Id { get; set; }
 
     public string Compass { get; set; } = null!;
 
+    [Column("Abbrevation")]
     public char Abbr { get; set; }
 }
