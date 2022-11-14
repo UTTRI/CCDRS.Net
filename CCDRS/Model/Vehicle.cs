6a7cfb8b0f7e4306582812e489875265a2bd21b6
amitsandhel <amit.sandhel@gmail.com>
2022-11-14 16:30:45 -0500

    Add Vehicle Model added vehicle model and service to the project

---
+++ CCDRS/Model/Vehicle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CCDRS.Model;
+
+/// <summary>
+/// Class that maps to the vehicle table
+/// </summary>
+public partial class Vehicle
+{
+    /// <summary>
+    /// Id is primary serial key of type int that is auto generated
+    /// </summary>
+    public int Id { get; set; }
+
+    /// <summary>
+    /// Stores the name of the vehicle, e.g. auto
+    /// </summary>
+    public string Name { get; set; } = null!;
+}
