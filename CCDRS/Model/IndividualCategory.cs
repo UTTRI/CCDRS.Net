305e38ab4f24a99a39aebf9ba3c7b23980a3c214
amitsandhel <amit.sandhel@gmail.com>
2023-08-18 11:06:50 -0400

    Refactored Database Schema and added the hierarchy to the front end Moved the methods responsible for filtering the individual category list into the individulCategories class. Using an enum for a filter over using static variables. removed the unneeded individualCategory table from the database.

--- CCDRS/Model/IndividualCategory.cs
+++ CCDRS/Model/IndividualCategory.cs
@@ -13,20 +13,51 @@
     along with CCDRS.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-using System;
-using System.Collections.Generic;
+using CCDRS.Data;
 
 namespace CCDRS.Model;
 
 /// <summary>
 /// Class that maps to the individual_categories view.
 /// </summary>
-public partial class IndividualCategory
+public sealed class IndividualCategory
 {
+    public IndividualCategory()
+    {
+
+    }
+
     /// <summary>
-    /// Primary serial key of type int that is auto generated
+    /// primary key of individual category table. 
+    /// It is needed for the DbSet in the contextservice to work.
     /// </summary>
     public int Id { get; set; }
+
+    /// <summary>
+    /// The number of occupants that can sit in a vehicle.
+    /// </summary>
+    public int OccupancyId { get; set; }
+
+    /// <summary>
+    /// Foreign key to the vehicle primary key.
+    /// </summary>
+    public int VehicleId { get; set; }
+
+    /// <summary>
+    /// Description of category to display to front end of user.
+    /// </summary>
+    public string? Description { get; set; } = string.Empty;
+
+    /// <summary>
+    /// The hierarchy order of which the individual categories will be displayed to the user.
+    /// </summary>
+    public int DisplayOrder { get; set; }
+
+    /// <summary>
+    /// The type of category if it is a total vehicle, total person or others. 
+    /// </summary>
+    public int CountType { get; set; }
+
 
     /// <summary>
     /// The name of the Vehicle
@@ -34,42 +65,83 @@ public partial class IndividualCategory
     public string VehicleName { get; set; } = string.Empty;
 
     /// <summary>
-    /// The number of occupants that can sit in a vehicle
-    /// </summary>
-    public int Occupancy { get; set; }
-
-    /// <summary>
-    /// Human readable description of the vehicle
-    /// </summary>
-    public string Description { get; set; } = string.Empty;
-
-    /// <summary>
     /// Foreign key to the vehicle_count_type table associated to the vehicle_count_type primary key attribute.
     /// </summary>
     public int VehicleCountTypeId { get; set; }
 
-    /// <summary>
-    /// Stores the type of vehicle. Used to determine drop down options
-    /// </summary>
-    public int CountType { get; set; }
+    enum TechnologyCount
+    {
+        TechnologyCounts = 1, // Filter all remaining technologies not the two below
+        VehicleCounts = 2, // Filter the total vehicle counts 
+        PersonCounts = 3 // Filter the total person counts 
+    }
 
     /// <summary>
-    /// Name of the region
+    /// Get the list of Individual categories that exist for a given survey.
     /// </summary>
-    public string RegionName { get; set; } = string.Empty;
+    /// <param name="selectedSurveyId">The surveyId to filter the subset of the technologies available.</param>
+    /// <param name="_context">The context service to access the database tables.</param>
+    /// <returns></returns>
+    public static IList<IndividualCategory> GetIndividualCategoriesBasedOnSurvey(int selectedSurveyId, CCDRSContext _context)
+    {
+        return (from vehiclecounttypes in _context.VehicleCountTypes
+                        join stationcountobserverations in _context.StationCountObservations on vehiclecounttypes.Id equals stationcountobserverations.VehicleCountTypeId
+                        join surveystations in _context.SurveyStations on stationcountobserverations.SurveyStationId equals surveystations.Id
+                        join surveys in _context.Surveys on surveystations.SurveyId equals surveys.Id
+                        join vehicles in _context.Vehicles on vehiclecounttypes.VehicleId equals vehicles.Id
+                        where
+                            surveys.Id == selectedSurveyId
+                        select new IndividualCategory()
+                        {
+                            OccupancyId = vehiclecounttypes.Occupancy,
+                            VehicleId = vehiclecounttypes.VehicleId,
+                            Description = vehiclecounttypes.Description,
+                            DisplayOrder = vehicles.DisplayOrder,
+                            CountType = vehiclecounttypes.CountType,
+                            VehicleName = vehicles.Name,
+                            VehicleCountTypeId = vehiclecounttypes.Id
+                        }).Distinct().OrderBy(s => s.DisplayOrder).ThenBy(s => s.OccupancyId).ToList();
+    }
 
     /// <summary>
-    /// Primary serial key of the rgion
+    /// General method to filter the individual category list based on the technology count type used to display
+    /// subset of categories in appropriate section to user.
     /// </summary>
-    public int RegionId { get; set; }
+    /// <param name="technologies">List of selected technologies for selected survey</param>
+    /// <param name="countType"></param>
+    /// <returns></returns>
+    private static IList<IndividualCategory> GetSpecificTechnologies(IList<IndividualCategory> technologies, int countType)
+    {
+        return technologies.Where(s => s.CountType == countType).ToList();
+    }
 
     /// <summary>
-    /// Year of which the data is selected for
+    /// Method to return the total vehicle count technologies for selected survey. 
     /// </summary>
-    public int Year { get; set; }
+    /// <param name="technologies">List of selected technologies for selected survey</param>
+    /// <returns>A list of the individual categories of total vehicles.</returns>
+    public static IList<IndividualCategory> GetTotalVehicleCounts(IList<IndividualCategory> technologies)
+    {
+        return GetSpecificTechnologies(technologies, (int)TechnologyCount.VehicleCounts);
+    }
 
     /// <summary>
-    /// Foreign key to the survey table associated to the survey primary key attribute
+    /// Method to return the total person count technologies for selected survey.
     /// </summary>
-    public int SurveyId { get; set; }
+    /// <param name="technologies">List of selected technologies for selected survey</param>
+    /// <returns></returns>
+    public static IList<IndividualCategory> GetTotalPersonCounts(IList<IndividualCategory> technologies)
+    {
+        return GetSpecificTechnologies(technologies, (int)TechnologyCount.PersonCounts);
+    }
+
+    /// <summary>
+    /// Method to return the technologies available for selected survey.
+    /// </summary>
+    /// <param name="technologies">List of selected technologies for selected survey</param>
+    /// <returns></returns>
+    public static IList<IndividualCategory> GetTechnologyCounts(IList<IndividualCategory> technologies)
+    {
+        return GetSpecificTechnologies(technologies, (int)TechnologyCount.TechnologyCounts);
+    }
 }
